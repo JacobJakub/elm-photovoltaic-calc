d694f3f084acc30768da9fab90ea5d98a31fe203
Marek Fajkus <marek.faj@gmail.com>
2018-02-06 01:28:19 +0100

    small refactoring of Battery module

--- Battery.elm
+++ Battery.elm
@@ -1,9 +1,12 @@
 module Battery exposing (Battery, recommend)
 
-import Usage exposing (Usage)
+import List exposing (head, map, repeat, sortBy, sum)
 import Load exposing (getAmps)
-import List exposing (sum, map, repeat, sortBy, head)
+import Usage exposing (Usage)
+
+
 -- MODEL
+
 
 type alias Battery =
     { name : String
@@ -12,24 +15,34 @@ type alias Battery =
     , price : Float
     }
 
-liFePO4 = Battery "LiFePO4" 80
 
 batteries : List Battery
 batteries =
+    let
+        liFePO4 =
+            Battery "LiFePO4" 80
+    in
     [ liFePO4 40 215.04
     , liFePO4 60 309.12
     , liFePO4 90 483.84
     ]
 
-recommend : Int -> List Usage -> Maybe ( List Battery )
+
+recommend : Int -> List Usage -> Maybe (List Battery)
 recommend volt load =
     let
-      getSmallestPack : Float -> Battery -> List Battery
-      getSmallestPack amps battery =
-          repeat ( ceiling ( amps / toFloat battery.ah ) ) battery
+        getSmallestPack : Float -> Battery -> List Battery
+        getSmallestPack amps ({ ah } as battery) =
+            (amps / toFloat ah)
+                |> ceiling
+                |> flip repeat battery
     in
-        head ( sortBy packPrice ( map ( getSmallestPack ( getAmps volt load ) ) batteries ) )
+    batteries
+        |> map (getSmallestPack <| getAmps volt load)
+        |> sortBy packPrice
+        |> head
+
 
 packPrice : List Battery -> Float
-packPrice batteryPack =
-    sum ( map .price batteryPack )
+packPrice =
+    sum << map .price
